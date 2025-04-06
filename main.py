de761112f41346f063c943211a68c3e0b759cb13
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2025-04-06 20:15:22 +0330

    Load the excel files into dataframe

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import pandas, fpdf
+import pandas as pd
+import glob
+
+filepaths = glob.glob("invoices/*.xlsx")
+
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name='Sheet 1')
